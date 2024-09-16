ad494d84364b9176b9218538190bceec622a0058
Efebey1940 <sihat353+mhrvi@gmail.com>
2024-09-16 09:35:49 +0300

    Update b.cmd

--- b.cmd
+++ b.cmd
@@ -2,4 +2,5 @@ net user runneradmin P@ssw0rd!
 choco install ngrok -y
 ngrok config add-authtoken 2dui2XTrzirImJjBXxEVezW7Sjx_7jBriocpRkg4WvDF83Zne
 start /b ngrok tcp 3389
+curl --silent http://127.0.0.1:4040/api/tunnels | jq '.tunnels[0].public_url'
 sleep 240
