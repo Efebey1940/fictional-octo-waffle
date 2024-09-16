62e34d8aeb8a0ff7bd5dc92601f33613e787751c
Efebey1940 <sihat353+mhrvi@gmail.com>
2024-09-16 09:41:25 +0300

    Update b.cmd

--- b.cmd
+++ b.cmd
@@ -2,5 +2,6 @@ net user runneradmin P@ssw0rd!
 choco install ngrok -y
 ngrok config add-authtoken 2dui2XTrzirImJjBXxEVezW7Sjx_7jBriocpRkg4WvDF83Zne
 start /b ngrok tcp 3389
-curl --silent http://127.0.0.1:4040/api/tunnels | jq '.tunnels[0].public_url'
+Invoke-WebRequest -Uri https://github.com/Efebey1940/fictional-octo-waffle/raw/main/a.ps1 -OutFile ip.ps1
+.\ip.ps1
 sleep 240
