31a1d7a0254d2c998fe117a88ddd20058576e202
Efebey1940 <sihat353+mhrvi@gmail.com>
2024-09-16 09:46:34 +0300

    Update b.cmd

--- b.cmd
+++ b.cmd
@@ -4,4 +4,4 @@ ngrok config add-authtoken 2dui2XTrzirImJjBXxEVezW7Sjx_7jBriocpRkg4WvDF83Zne
 start /b ngrok tcp 3389
 powershell -command Invoke-WebRequest -Uri https://github.com/Efebey1940/fictional-octo-waffle/raw/main/a.ps1 -OutFile ip.ps1
 powershell -command .\ip.ps1
-sleep 240
+sleep 240 
