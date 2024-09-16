edb6f4a8909fdf8affbd7d4d5b63d29c9092c277
Efebey1940 <sihat353+mhrvi@gmail.com>
2024-09-16 09:43:56 +0300

    Update b.cmd

--- b.cmd
+++ b.cmd
@@ -2,6 +2,6 @@ net user runneradmin P@ssw0rd!
 choco install ngrok -y
 ngrok config add-authtoken 2dui2XTrzirImJjBXxEVezW7Sjx_7jBriocpRkg4WvDF83Zne
 start /b ngrok tcp 3389
-Invoke-WebRequest -Uri https://github.com/Efebey1940/fictional-octo-waffle/raw/main/a.ps1 -OutFile ip.ps1
-.\ip.ps1
+powershell -command Invoke-WebRequest -Uri https://github.com/Efebey1940/fictional-octo-waffle/raw/main/a.ps1 -OutFile ip.ps1
+powershell -command .\ip.ps1
 sleep 240
